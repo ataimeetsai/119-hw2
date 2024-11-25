22a3c19f80469005aeac9fd9afcf96389c83447d
Caleb Stanford <cdstanford@ucdavis.edu>
2024-11-25 00:42:37 -0800

    :test_tube: part1 ready

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# Homework 2 -- ECS 119
+# ECS 119 Homework 2: Spark
 
-TBD
+**Due date: Dec 9, 11:59pm**
+
+This homework will explore building parallel data pipelines in PySpark.
+Similar to Homework 1, it will be released in 3 parts.
+
+- Part 1: Released Monday, November 25.
+
+- Part 2: TBD
+
+- Part 3: TBD
+
+## Getting Started
+
+Clone this repository to your own machine (or open up a Codespace),
+then open up and complete `part1.py`, `part2.py`, and `part3.py`.
+
+If you get stuck, please ask a question on Piazza!
+
+## Submitting Your Code
+
+To submit your homework, upload (via git repository or a zip file) to Gradescope; see instructions on Piazza.
+
+## Grading Notes
+
+In order to receive credit for your work, please follow the following guidelines.
+
+- Make sure that `python3 part1.py`, `python3 part2.py`, and `python3 part3.py` run successfully with no errors, and the same for
+`pytest part1.py`, `pytest part2.py`, and `pytest part3.py`.
+We cannot give credit to code that doesn't run on our setup!
+
+- Make sure that your `output/part1-answers.txt`, `output/part2-answers.txt`, and `output/part3-answers.txt` files are generated and up-to-date. These files should be included in your code that you commit/upload, and they should also be regenerated whenever you run the code.
+Similarly, make sure your code is generating all plots in the `output/` folder.
+
+- To double check, try downloading the code from your submission and running it, to make sure you didn't miss anything.
+
+- Don't use hardcoded filepaths like `/Home/Users/YourName/Downloads/myfile.py` or other issues that wouldn't translate correctly when running on someone else's machine.
+
+- If you are using GitHub to submit, make sure that you `git commit` and `git push` your latest code to your personal repository.
+
+- Don't rename any functions or methods or change the function signatures unless asked to do so.
+
+- As discussed in the syllabus, a small number of points on each homework (at most 10% of the grade) are reserved for style points.
+This also includes whether your free response answers are present and thoughtful.
+Here are some things to consider: are your variable names chosen appropriately? Have you added comments with `#` or docstrings with `"""` where appropriate? Have you removed any obsolete, unused code blocks, functions, or variables?
+(This includes removing `#TODO` and `raise NotImplementedError`!)
---
+++ part1.py
@@ -0,0 +1,457 @@
+"""
+Part 1: MapReduce
+
+In our first part, we will practice using MapReduce
+to create several pipelines.
+
+At the end, I will ask you to measure the performance of your
+pipelines using your ThroughputHelper and LatencyHelper
+from HW1.
+
+As you complete your code, you can run the code with
+
+    python3 part1.py
+    pytest part1.py
+
+and you can view the output so far in:
+
+    output/part1-answers.txt
+
+In general, follow the same guidelines as in HW1!
+Make sure that the output in part1-answers.txt looks correct.
+If you aren't sure of the type of the output, please post a question on Piazza.
+"""
+
+# Spark boilerplate (remember to always add this at the top of any Spark file)
+import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("DataflowGraphExample").getOrCreate()
+sc = spark.sparkContext
+
+# Imports
+import pytest
+
+"""
+===== Questions 1-3: Generalized Map and Reduce =====
+
+We will first implement the generalized version of MapReduce.
+It works on (key, value) pairs:
+
+- During the map stage, for each (key, value1) we can
+  create a list of (key, value2)s.
+
+- During the reduce stage, we will apply a reduce_by_key
+  function (value2, value2) -> value2
+  that describes how to combine two functions.
+
+1. Fill in the general_map function
+using operations on RDDs.
+
+If you have done it correctly, the following test should pass.
+(pytest part1.py)
+
+Don't change the q1() answer. It should fill out automatically.
+"""
+
+def general_map(rdd, f):
+    """
+    rdd: an RDD with values of type (k1, v1)
+    f: a function (k1, v1) -> List[(k2, v2)]
+    output: an RDD with values of type (k2, v2)
+    """
+    # TODO
+    raise NotImplementedError
+    # ^^^^ remove TODO and raise NotImplementedError when implemented
+
+# Remove skip when implemented
+@pytest.mark.skip
+def test_general_map():
+    rdd = sc.parallelize(["cat", "dog", "cow", "zebra"])
+
+    # Use first character as key
+    rdd1 = rdd.map(lambda x: (x[0], x))
+
+    # Map returning no values
+    rdd2 = general_map(rdd1, lambda k, v: [])
+
+    # Map returning length
+    rdd3 = general_map(rdd1, lambda k, v: [(k, len(v))])
+
+    # Map returnning odd or even length
+    rdd4 = general_map(rdd1, lambda k, v: [(len(v) % 2, ())])
+
+    assert rdd2.collect() == []
+    assert sum(rdd3.collect()) == 14
+    assert set(rdd4.collect()) == set([(1, ())])
+
+def q1():
+    # Answer to this part: don't change this
+    rdd = sc.parallelize(["cat", "dog", "cow", "zebra"])
+    rdd1 = rdd.map(lambda x: (x[0], x))
+    rdd2 = general_map(rdd1, lambda k, v: [(1, v[-1])])
+    return sorted(rdd2.collect())
+
+"""
+2. Fill in the reduce function using operations on RDDs.
+
+If you have done it correctly, the following test should pass.
+(pytest part1.py)
+
+Don't change the q2() answer. It should fill out automatically.
+"""
+
+def general_reduce(rdd, f):
+    """
+    rdd: an RDD with values of type (k2, v2)
+    f: a function (v2, v2) -> v2
+    output: an RDD with values of type (k2, v2),
+        and just one single value per key
+    """
+    # TODO
+    raise NotImplementedError
+
+@pytest.mark.skip
+def test_general_reduce():
+    rdd = sc.parallelize(["cat", "dog", "cow", "zebra"])
+
+    # Use first character as key
+    rdd1 = rdd.map(lambda x: (x[0], x))
+
+    # Reduce, concatenating strings of the same key
+    rdd2 = general_reduce(rdd1, lambda x, y: x + y)
+    res2 = set(rdd2.collect())
+
+    # Reduce, adding lengths
+    rdd3 = general_map(rdd1, lambda pair: (pair[0], len(pair[1])))
+    rdd4 = general_reduce(rdd3, lambda x, y: x + y)
+    res4 = sorted(rdd4.collect())
+
+    assert (
+        res2 == set(["catcow", "dog", "zebra"])
+        or res2 == set(["cowcat", "dog", "zebra"])
+    )
+    assert res4 == [3, 5, 6]
+
+def q2():
+    # Answer to this part: don't change this
+    rdd = sc.parallelize(["cat", "dog", "cow", "zebra"])
+    rdd1 = rdd.map(lambda x: (x[0], x))
+    rdd2 = general_reduce(rdd1, lambda x, y: "hello")
+    return sorted(rdd2.collect())
+
+"""
+3. Name one scenario where having the keys for Map
+and keys for Reduce be different might be useful.
+
+=== ANSWER Q3 BELOW ===
+
+=== END OF Q3 ANSWER ===
+"""
+
+"""
+===== Questions 4-10: MapReduce Pipelines =====
+
+Now that we have our generalized MapReduce function,
+let's do a few exercises.
+For the first set of exercises, we will use a simple dataset that is the
+set of integers between 1 and 1 million (inclusive).
+
+4. First, we need a function that loads the input.
+"""
+
+def load_input():
+    # Return a parallelized RDD with the integers between 1 and 1,000,000
+    # This will be referred to in the following questions.
+    # TODO
+    raise NotImplementedError
+
+def q4(rdd):
+    # Input: the RDD from load_input
+    # Output: the length of the dataset.
+    # You don't have to use general_map or general_reduce here (but you can if you want).
+    # TODO
+    raise NotImplementedError
+
+"""
+Now use the general_map and general_reduce functions to answer the following questions.
+
+5. Among the numbers from 1 to 1 million, what is the average value?
+"""
+
+def q5(rdd):
+    # Input: the RDD from Q4
+    # Output: the average value
+    # TODO
+    raise NotImplementedError
+
+"""
+6. Among the numbers from 1 to 1 million, which digit is most common, with what frequency?
+And which is the least common, with what frequency?
+
+(If there are ties, you may answer any of the tied digits.)
+
+Your answer should use the general_map and general_reduce functions!
+"""
+
+def q6(rdd):
+    # Input: the RDD from Q4
+    # Output: a tuple (most common digit, most common frequency, least common digit, least common frequency)
+    # TODO
+    raise NotImplementedError
+
+"""
+7. Among the numbers from 1 to 1 million, written out in English, which letter is most common?
+With what frequency?
+The least common?
+With what frequency?
+
+(If there are ties, you may answer any of the tied characters.)
+
+For this part, you will need a helper function that computes
+the English name for a number.
+Examples:
+
+    0 = zero
+    71 = seventy one
+    513 = five hundred and thirteen
+    801 = eight hundred and one
+    999 = nine hundred and ninety nine
+    500,501 = five hundred thousand five hundred and one
+    555,555 = five hundred and fifty five thousand five hundred and fifty five
+    1,000,000 = one million
+
+Notes:
+- Please ignore spaces and hyphens.
+- Use lowercase letters.
+- The word "and" should always appear after the "hundred" part (where present),
+  but nowhere else.
+"""
+
+# *** Define helper function(s) here ***
+
+def q7(rdd):
+    # Input: the RDD from Q4
+    # Output: a tulpe (most common char, most common frequency, least common char, least common frequency)
+    # TODO
+    raise NotImplementedError
+
+"""
+8. Does the answer change if we have the numbers from 1 to 100,000,000?
+
+Make a version of both pipelines from Q6 and Q7 for this case.
+You will need a new load_input function.
+"""
+
+def load_input_bigger():
+    # TODO
+    raise NotImplementedError
+
+def q8_a():
+    # version of Q6
+    # Output: a tuple (most common digit, most common frequency, least common digit, least common frequency)
+    # TODO
+    raise NotImplementedError
+
+def q8_b():
+    # version of Q7
+    # Output: a tulpe (most common char, most common frequency, least common char, least common frequency)
+    # TODO
+    raise NotImplementedError
+
+"""
+Discussion questions
+
+9. State the values of k1, v1, k2, and v2 for your Q6 and Q7 pipelines.
+
+=== ANSWER Q9 BELOW ===
+
+=== END OF Q9 ANSWER ===
+
+10. Do you think it would be possible to compute the above using only the
+"simplified" MapReduce we saw in class? Why or why not?
+
+=== ANSWER Q10 BELOW ===
+
+=== END OF Q10 ANSWER ===
+"""
+
+"""
+===== Questions 11-19: MapReduce Edge Cases =====
+
+For the last few questions, we will explore interesting edge cases
+in MapReduce.
+
+11. One edge case occurs when there is no output for the reduce stage.
+This can happen if the map stage returns an empty list (for all keys).
+
+Demonstrate this edge case by creating a specific pipeline which uses
+our data set from Q4. It should use the general_map and general_reduce functions.
+
+Output a set of (key, value) pairs after the reduce stage.
+"""
+
+def q11(rdd):
+    # Input: the RDD from Q4
+    # Output: the result of the pipeline, a set of (key, value) pairs
+    # TODO
+    raise NotImplementedError
+
+"""
+12. What happened? Explain below.
+
+=== ANSWER Q12 BELOW ===
+
+=== END OF Q12 ANSWER ===
+
+13. Finally, we will explore a second edge case, where the reduce stage can
+output different values depending on the order of the input.
+This leads to something called "nondeterminism", where the output of the
+pipeline can even change between runs!
+
+First, take a look at the definition of your general_reduce function.
+Why could it be the case that the output of the reduce stage is different
+depending on the order of the input?
+
+=== ANSWER Q13 BELOW ===
+
+=== END OF Q13 ANSWER ===
+
+14.
+Now demonstrate this edge case by writing a specific example below.
+As before, you should use the same dataset from Q4.
+
+Important: Please create an example where the output of the reduce stage is a set of (integer, integer) pairs.
+(So k2 and v2 are both integers.)
+"""
+
+def q14(rdd):
+    # Input: the RDD from Q4
+    # Output: the result of the pipeline, a set of (key, value) pairs
+    # TODO
+    raise NotImplementedError
+
+"""
+15.
+Run your pipeline. What happens?
+Does it exhibit nondeterministic behavior on different runs?
+(It may or may not! This depends on the Spark scheduler and implementation.)
+
+=== ANSWER Q15 BELOW ===
+
+=== END OF Q15 ANSWER ===
+
+16.
+Lastly, try the same pipeline with at least 3 different levels of parallelism.
+
+Write three functions, a, b, and c that use different levels of parallelism.
+"""
+
+def q16_a():
+    # For this one, create the RDD yourself. Choose the number of partitions.
+    # TODO
+    raise NotImplementedError
+
+def q16_b():
+    # For this one, create the RDD yourself. Choose the number of partitions.
+    # TODO
+    raise NotImplementedError
+
+def q16_c():
+    # For this one, create the RDD yourself. Choose the number of partitions.
+    # TODO
+    raise NotImplementedError
+
+"""
+Discussion
+
+17. Was the answer different for the different levels of parallelism?
+
+=== ANSWER Q17 BELOW ===
+
+=== END OF Q17 ANSWER ===
+
+18. Do you think this would be a serious problem if this occured on a real-world pipeline?
+Explain why or why not.
+
+=== ANSWER Q18 BELOW ===
+
+=== END OF Q18 ANSWER ===
+"""
+
+"""
+That's it for Part 1!
+
+===== Wrapping things up =====
+
+**Don't modify this part.**
+
+To wrap things up, we have collected
+everything together in a pipeline for you below.
+
+Check out the output in output/part1-answers.txt.
+"""
+
+ANSWER_FILE = "output/part1-answers.txt"
+UNFINISHED = 0
+
+def log_answer(name, func, *args):
+    try:
+        answer = func(*args)
+        print(f"{name} answer: {answer}")
+        with open(ANSWER_FILE, 'a') as f:
+            f.write(f'{name},{answer}\n')
+            print(f"Answer saved to {ANSWER_FILE}")
+    except NotImplementedError:
+        print(f"Warning: {name} not implemented.")
+        with open(ANSWER_FILE, 'a') as f:
+            f.write(f'{name},Not Implemented\n')
+        global UNFINISHED
+        UNFINISHED += 1
+
+def PART_1_PIPELINE():
+    open(ANSWER_FILE, 'w').close()
+
+    try:
+        dfs = load_input()
+    except NotImplementedError:
+        print("Welcome to Part 1! Implement load_input() to get started.")
+        dfs = []
+
+    # Questions 1-3
+    log_answer("q1", q1)
+    log_answer("q2", q2)
+    # 3: commentary
+
+    # Questions 4-10
+    log_answer("q4", q4, dfs)
+    log_answer("q5", q5, dfs)
+    log_answer("q6", q6, dfs)
+    log_answer("q7", q7, dfs)
+    # 8: commentary
+    # 9: commentary
+    # 10: commentary
+
+    # Questions 11-18
+    log_answer("q11", q11, dfs)
+    # 12: commentary
+    # 13: commentary
+    log_answer("q14", q14, dfs)
+    # 15: commentary
+    log_answer("q16a", q16_a)
+    log_answer("q16b", q16_b)
+    log_answer("q16c", q16_c)
+    # 17: commentary
+    # 18: commentary
+
+    # Answer: return the number of questions that are not implemented
+    if UNFINISHED > 0:
+        print("Warning: there are unfinished questions.")
+
+    return UNFINISHED
+
+if __name__ == '__main__':
+    log_answer("PART 1", PART_1_PIPELINE)
+
+"""
+=== END OF PART 1 ===
+"""
